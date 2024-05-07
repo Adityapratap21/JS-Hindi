45113d156b534429a201a2de9213d79e42b2f637
Adityapratap21 <138037272+Adityapratap21@users.noreply.github.com>
2024-05-07 00:09:31 +0000

    some new thing addwd in this file

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,4 +1,34 @@
 const name = "Aditya"
 const repoCount = 50
 
-console.log(name +repoCount +"Value");
+// console.log(name +repoCount +"Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('aditya-aps')
+
+console.log(gameName[0]);
+console.log((gameName.__proto__));
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "   aditya   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://aditya.com/aditya%20pratap"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
